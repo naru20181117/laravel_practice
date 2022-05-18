86a5404a4b4600752d6eeac1a68a19dd99fd467b
なるpro <t.naruhiro.1026@gmail.com>
2022-05-18 12:14:08 +0900

    add: modelの追加

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use App\Models\Employee;
 
 class EmployeesController extends Controller
 {
   public function index()
   {
-    $employees = DB::table('employees')->get();
+    $employees = Employee::all();
 
     return view('employees.index', ['employees' => $employees]);
   }
@@ -22,11 +23,11 @@ class EmployeesController extends Controller
 
   public function store(Request $request)
   {
-    DB::table('employees')->insert([
-      'employee_name' => $request->input('employee_name'),
-      'section' => $request->input('section'),
-      'salary' => $request->input('salary'),
-    ]);
+    $employee = new Employee;
+    $employee->employee_name = $request->input('employee_name');
+    $employee->section = $request->input('section');
+    $employee->salary = $request->input('salary');
+    $employee->save();
 
     return view('employees.store');
   }
